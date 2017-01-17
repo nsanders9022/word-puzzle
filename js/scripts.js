f6e59eeb2f71567e7e514ae9c3cf4dac0f3f5e74
Caitlin Hines and Nicole Sanders <caitlin@>
2017-01-17 10:36:50 -0800

    include looping in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,24 @@ $("document").ready(function(){
 
     var sentenceInput = $("#sentence").val();
 
-    $(".result").append(sentence(sentenceInput));
+    $(".result").text(sentence(sentenceInput));
 
   });
 });
+
+var sentence = function(sentenceInput) {
+  var sentArrays = sentenceInput.split("");
+  var resultArray = [];
+
+  sentArrays.forEach(function(letter) {
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u"){
+      resultArray.push("-");
+    } else {
+      resultArray.push(letter);
+    }
+
+  });
+
+  var resultString = resultArray.join("");
+  return resultString;
+}
